304e8b6fdfb955512cbe2c26e910e2b50e1a7eec
George Nabil <georgenabilpro@gmail.com>
2022-12-14 00:26:02 +0200

    adding the topics list nav menu

--- src/Components.js
+++ src/Components.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import { useState } from 'react'
+import $ from 'jquery'
+import _ from 'underscore'
+
+import {useEffect, useMemo, useState, useId } from 'react'
 import {MathJaxContext, MathJax} from 'better-react-mathjax'
-import {Link} from 'react-router-dom'
+import {Link, useLocation, matchRoutes} from 'react-router-dom'
+import {HashLink} from 'react-router-hash-link'
+
 const PATH1 = '../assets/manim/'
 const PATH2 = '../assets/'
-/*
-t --> title
-h --> header
-p --> path
-*/
-
+const cTypes = {
+    't': ['Theorem', 'theorem'],
+    'e': ['Example', 'example'],
+    's': ['Solving stratigy', 'stratigy'],
+    'n': ['Notes', 'note'],
+    'p': ['Proof', 'proof'],
+    'd': ['Definition', 'definition'],
+    'r': ['Rule', 'rule'],
+    'further' : ['Further knowledge', 'further'],
+}
 
 function Chapter(props) {
+    let preContentNav = <ContentNav sm={props.children}/>
     return (
         <>
+            {preContentNav}
             <div className={'main n'+ props.n}>
                 <h2>
                     <span className='before'>{props.n}</span>
@@ -31,88 +42,61 @@ function Chapter(props) {
 }
 
 const SM = (props)=> {
-  return (
-    <section className="section1">
-      <h3>{props.h}</h3>
-      {props.children}
-    </section>
-  )
+    // Takes < 
+    //      h:header  !! , !UNIQUE, 
+    const id = props.h.toLowerCase().replaceAll(' ','_')
+    return (
+        <section className="section1" id={id}>
+            <h3>{props.h}</h3>
+            {props.children}
+        </section>
+    )
 }
 
 const S = (props)=> {
+    // Takes < 
+    //      h:header  !! , !UNIQUE,
+    const id = props.h.toLowerCase().replaceAll(' ','_')
     return (
-        <div className="section2">
+        <section className="section2" id={id}>
             <h4><span>&#8811; </span> {props.h}</h4>
-            
                 {props.children}
-
-        </div>
+        </section>
     )
 }
 
-function C(props) {
-    let config = ['Theorem:', 'theorem']
-    switch (props.t){
-        case 't':
-            config = ['Theorem:', 'theorem']
-            break
-        case 'e':
-            config = ['Example:', 'example']
-            break
-        case 's':
-            config = ['Solving stratigy:', 'stratigy']
-            break
-        case 'n':
-            config = ['Notes', 'note']
-            break
-        case 'p':
-            config = ['Proof', 'proof']
-            break
-        case 'further':
-            config = ['Further knowledge', 'further']
-            break
-        case 'r':
-            config = ['Rule:', 'rule']
-            break
-        case 'd':
-            config = ['Definition:', 'definition']
-            break
+class C extends React.Component {
+    // Takes < 
+    //    h:title             #= ""
+    //    id:id               !!, !UNIQUE
+    //    t:type {cTypes}     #= "t"
+    constructor(props) {
+        super(props);
+        this.id = props.id
+        this.title = props.h? ': '+props.h : ':';
+        this.children = props.children;
+        this.config = ['Theorem:', 'theorem'];
+        if (props.t in cTypes) {
+            this.config = cTypes[props.t];
+        }
     }
-
-    const title = props.h? props.h:'';
-
-    return (
-        <div className='c-holder'>
-{/*         { props.t !== 'e' ? 
-            <div className={config[1] + ' c'}>
-                <h5 className='title'>
-                    <span>{config[0]}</span>{' ' + title}
-                </h5>
-                <div className="content">
-                    {props.children}
+    
+    render() {
+        return (
+            <div className='c-holder ' id={this.id}>
+                <div className={this.config[1] + ' c'}>
+                    <h5 className='title'>
+                        <span>{this.config[0]}</span>{this.title}
+                    </h5>
+                    <div className="content">
+                        {this.children}
+                    </div>
                 </div>
             </div>
-            : 
-            <div className={config[1] + ' c'}>
-                <h5 className='title'>
-                    <span>{config[0]}</span>{' ' + title}
-                </h5>
-                <div className="content">
-                    {props.children}
-                </div>
-            </div>
-        }  */}
-            <div className={config[1] + ' c'}>
-                <h5 className='title'>
-                    <span>{config[0]}</span>{' ' + title}
-                </h5>
-                <div className="content">
-                    {props.children}
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 const U1 = (props)=> {
     return (
@@ -123,65 +107,124 @@ const U1 = (props)=> {
 }
 
 const A = (props) => {
-    console.log("#" + props.r)
+    // Takes < 
+    //      r:route {1,2,3,4,5}           #= 3, 
+    //          1: /background-and-basics'
+    //          2: /techniques-of-integration'
+    //          3: /multiple-integration'
+    //          4: /vector-calculus'
+    //          5: /applications-of-integration'
+    //      id:                           !!
+    const route = props.r? props.r: "/multiple-integration"
     return (
-        <a href={'#' + props.r} className="local-reference">
+        <HashLink to={route + '#' + props.id} className="local-reference">
             {props.children}
-        </a>
+        </HashLink>
     )
 }
 
 const Mimg = (props) => {
+    // Takes < 
+    //      h:title, caption  #= "", 
+    //      t:type {'g',''}   #= "",
+    //          'g': To take source from assets directory (PATH2)
+    //          '' : To take source from assets/manim directory (PATH1)
+    //      p:path            !!,
     let path = props.t === 'g'?  PATH2 + props.p : PATH1 + props.p 
     return (
-        <img src={require(path)}/>
+        <figure>
+            <img src={require(path)}/>
+            <figcaption>{props.h}</figcaption>
+        </figure>
     )
 }
 
+const ContentNav = (props) => {
+    // Takes <
+    //    sm:main sections list     !AUTO
+    const location = useLocation().pathname;
+    const SMElements = props.sm;
+    let order = {'r':0, 't':0, 's':0, 'n':0, 'p':0, 'e':0, 'd':0, 'further':0};
 
-const Header = ()=> {
+    let ordered_ = useMemo(()=>{
+        let nKey = 0
+        let ordered = []
+        for (let el of SMElements) {
+            // Here each el is SM component
+            nKey ++;
+            let id = el.props.h.toLowerCase().replaceAll(' ','_')
+            ordered.push(
+                <li className={'content-main-section'} key={nKey}>
+                    <HashLink smooth to={`#${id}`}>
+                        {el.props.h}
+                    </HashLink>
+                </li>
+            )
+            let elChildren = el.props.children
+
+            if (elChildren) {
+                if (elChildren.constructor !== Array) {
+                    if (elChildren.constructor === Object) {
+                        elChildren = [elChildren]
+                    } else continue
+                }
+            } else continue
+
+            for (let child1 of elChildren) {
+                // Here each child1 is a S component
+                if (child1.props) {
+                    nKey ++;
+                    let id = child1.props.h.toLowerCase().replaceAll(' ','_')
+                    ordered.push(
+                        <li className={'content-section'} key={nKey}>
+                            <HashLink smooth to={`#${id}`}>
+                                {child1.props.h}
+                            </HashLink>
+                        </li>
+                    );
+                    let children = child1.props.children
+                    
+                    if (children) {
+                        if (children.constructor !== Array) {
+                            if (children.constructor === Object) {
+                                children = [children]
+                            } else continue
+                        }
+                    } else continue
+
+                    for (let child2 of children) {
+                        // Here each child2 is a C component
+                        if (child2.props) {
+                            let type = child2.props.t;
+                            if (type) {
+                                order[type] ++; nKey ++;
+                                ordered.push(
+                                    <li className={`content-container`} key={'c' + child2.props.id}>
+                                        <HashLink smooth to={`#${child2.props.id}`}>
+                                            {cTypes[type][0]}, {order[type]}
+                                        </HashLink>
+                                    </li>
+                                )
+                            } 
+                        } 
+                    }
+                } 
+            }
+        }
+        return ordered
+    },[])
     return (
-    <header id='main-title'>
-            <h1>
-                Hyper Calculus
-            </h1>
-            <h3>Your Integral Calculus Reference</h3>
-
-    </header>
-    )
-}
-
-const Footer = ()=> {
-    return (
-    <footer>
-        <div id='disclaimer' style={{"maxWidth":"70ch"}}>
-            <p>
-                <strong>What is the purpose of this work?</strong>
-                <br />
-                This Reference aims to help students find all calculus formulas fast, And to be as a repository for calculus tips and tricks and stratigies of solving. And we expect you to have the necessary knowledge before you use this reference.
-            </p>
-            <br />
-            <p>
-                If you have any questions or suggestions or you want to join owr team for developing this reference, you can contact me:<br />
-                Gmail: <a href="mailto:georgenabilpro@gmail.com"><strong>georgenabilpro@gmail.com</strong></a><br/>
-                Whatsapp: <a href="tel:01014374579"><strong>(+20)01014374579</strong></a>
-            </p>
-        </div>
-        <div id="credits">
-            <h6>Content writing: </h6> <h5>G.N.</h5>
-            <h6>Website programming: </h6> <h5>G.N.</h5>
-            <h6>Designing: </h6> <h5>G.N.</h5>
-            <h6>Revising: </h6> <h5>G.N.</h5>
-            <h6>Editing: </h6> <h5>G.N.</h5>
-            <h6>Publishing: </h6> <h5>G.N.</h5>
-            <h6>Videos by: </h6> <h5>G.N.</h5>
-            <h6>Images by:</h6> <h5>G.N.</h5>
-        </div>
-    </footer>
+        <aside className='contents-menu'>
+            <ul>
+                {[...ordered_]}
+            </ul>
+        </aside>
     )
 }
 
 const Navigation = (props) => {
+    // Takes <
+    //    choose:the tab which is activated at the moment  !AUTO
     const c = props.choose
     const [isActive, setIsActive] = useState(false);
 
@@ -226,17 +269,9 @@ const Navigation = (props) => {
 }
 
 const CONFIG = {
+    // The MathJax configuration block
     jax: ["input/TeX","output/SVG"],/*, "output/PreviewHTML"*/
     extensions: ["tex2jax.js", "TeX/AMSmath.js","mml2jax.js","MathEvents.js"],
-/*     SVG: {
-        fontCache: 'global',
-        mtextFontInherit: true,
-        blacker: 1,
-        linebreaks: { automatic: true }
-    },
-    loader: { 
-        load: ['input/tex-base', 'output/svg', '[tex]/require', '[tex]/mathtools'] //, 'ui/menu'
-    }, */
     tex: {
       inlineMath: [["$", "$"],['"','"']],
       displayMath: [["$$", "$$"]],
@@ -254,22 +289,8 @@ const CONFIG = {
         availableFonts: ["STIX","TeX"],  
     }
 };
-/*
-
-  menuSettings: {
-  zoom: "Click"
-  },
-  MatchWebFonts: {
-  matchFor: {
-    SVG: true
-  },
-  fontCheckDelay: 500,
-  fontCheckTimeout: 15 * 1000
-  },
-  messageStyle: "none"
-  });
-*/
 
 export {
     S, SM, Chapter, C, A, CONFIG, U1, Mimg,
-    Header, Footer, Navigation}
+    Navigation, ContentNav
+}
